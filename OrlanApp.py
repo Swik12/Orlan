722744c962079da3e211239789be3e22584469b1
Orrin <svikaorrin2019@gmail.com>
2023-08-15 21:50:55 -0500

    Commit1

--- OrlanApp.py
+++ OrlanApp.py
@@ -1,9 +1,43 @@
-print("Hello World")
-print("This is the second change.")
-print("Still no clue")
-print("Now I am on a new branch")
-print("Hello")
-#Commit 1
-print("hello")
-print("What is happening")
-#I have absolutely no clue what is happening.
+import pygame
+
+class Drone:
+    def __init__(self, drone_type, specs):
+        self.drone_type = drone_type
+        self.specs = specs
+
+class DroneController:
+    def __init__(self):
+        self.drones = []
+
+    def add_drone(self, drone_type, specs):
+        new_drone = Drone(drone_type, specs)
+        self.drones.append(new_drone)
+
+class RemoteControllerApp:
+    def __init__(self):
+        self.controller = DroneController()
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 600))
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):
+        while self.running:
+            self.handle_events()
+            self.update_screen()
+            self.clock.tick(30)
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            # Handle Xbox controller input here
+
+    def update_screen(self):
+        self.screen.fill((255, 255, 255))  # White background
+        # Draw UI elements (buttons, drone list, etc.)
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    app = RemoteControllerApp()
+    app.run()
